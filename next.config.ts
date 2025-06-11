a0de66f5dbb3c427279078905f37ea7f80335f99
Adnan Khan <IGNITION420@GMAIL.COM>
2025-06-11 18:51:32 +0500

    projects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [new URL("https://placehold.co/**")],
+  }
 };
 
 export default nextConfig;
